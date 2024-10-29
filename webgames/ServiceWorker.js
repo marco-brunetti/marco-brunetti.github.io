41c67698be8d9787352d0a474a915ce51669de8c
Marco <marco19-96@hotmail.com>
2024-10-29 00:51:25 -0300

    Added test webgame

---
+++ webgames/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-tiny_boats-0.1.0";
+const contentToCache = [
+    "Build/tb_build.loader.js",
+    "Build/tb_build.framework.js.unityweb",
+    "Build/tb_build.data.unityweb",
+    "Build/tb_build.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
