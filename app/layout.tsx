408f3df0936ecfdd15c218035066d156650bc467
Marco <marco19-96@hotmail.com>
2025-06-09 00:36:21 -0300

    Fixed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Marco Brunetti',
+  title: 'Marco Brunetti | Game Developer Portfolio',
   description: 'Game Developer Portfolio',
 }
 
